3f2c064feb53d5599c4914e97ea05d166ef7c039
Anna Borzenko <annnechko@gmail.com>
2024-01-29 23:08:16 +1300

    add default controller, home leads to students list

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('', name: 'home')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_student_index');
+    }
+}
