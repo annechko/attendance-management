b2b58def1f092d16dbb09c0bb34ca5448c9960cd
Butch Dela Cruz <butchdc@gmail.com>
2024-02-19 15:33:03 +1300

    Changes to Admin Dashboard

--- app/src/Controller/Admin/HomeController.php
+++ app/src/Controller/Admin/HomeController.php
@@ -5,13 +5,39 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AdminRepository;
+use App\Repository\StudentRepository;
+use App\Repository\TeacherRepository;
+use App\Repository\InstitutionRepository;
+use App\Repository\CourseRepository;
+use App\Repository\SubjectRepository;
+use App\Repository\IntakeRepository;
+use App\Repository\PeriodRepository;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/admin', name: 'admin_home')]
-    public function index(): Response
+    public function index(
+        AdminRepository $adminrepo,
+        StudentRepository $studentrepo,
+        TeacherRepository $teacherrepo,
+        InstitutionRepository $institutionrepo,
+        CourseRepository $coursesrepo,
+        SubjectRepository $subjectsrepo,
+        IntakeRepository $intakesrepo,
+        PeriodRepository $periodsrepo,
+        ): Response
     {
         return $this->render('admin/index.html.twig', [
+            'administrators' => $adminrepo->count([]),
+            'students' => $studentrepo->count([]),
+            'teachers' => $teacherrepo->count([]),
+            'institutions' => $institutionrepo->count([]),
+            'courses' => $coursesrepo->count([]),
+            'subjects' => $subjectsrepo->count([]),
+            'intakes' => $intakesrepo->count([]),
+            'periods' => $periodsrepo->count([]),
         ]);
     }
 }
