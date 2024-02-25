3a466b2aff95e5bc69cace9df341318ce9c715fd
Butch Dela Cruz <butchdc@gmail.com>
2024-02-25 21:28:01 +1300

    new show ui

--- app/src/Form/IntakeType.php
+++ app/src/Form/IntakeType.php
@@ -8,19 +8,35 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class IntakeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('start')
-            ->add('finish')
             ->add('course', EntityType::class, [
                 'class' => Course::class,
                 'choice_label' => 'name',
-            ]);
+            ])
+            ->add('name')
+            ->add('start')
+            ->add('finish')
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $intake = $event->getData();
+                $form = $event->getForm();
+                if ($intake->getCourse() !== null) {
+                    $form->add('course', EntityType::class, [
+                        'class' => Course::class,
+                        'choice_label' => 'name',
+                        'disabled' => true, // Set disabled attribute
+                        'placeholder' => 'Select an Intake', // Placeholder text
+                        'required' => false,
+                    ]);
+                }
+            });
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- app/src/Form/PeriodType.php
+++ app/src/Form/PeriodType.php
@@ -8,19 +8,34 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class PeriodType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('start')
-            ->add('finish')
             ->add('intake', EntityType::class, [
                 'class' => Intake::class,
                 'choice_label' => 'name',
-            ]);
+            ])
+            ->add('name')
+            ->add('start')
+            ->add('finish')
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $period = $event->getData();
+                $form = $event->getForm();
+
+                if ($period->getIntake() !== null) {
+                    $form->add('intake', EntityType::class, [
+                        'class' => Intake::class,
+                        'choice_label' => 'name',
+                        'disabled' => true, // Set disabled attribute
+                        'required' => false, // Make the field not required
+                    ]);
+                }
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
