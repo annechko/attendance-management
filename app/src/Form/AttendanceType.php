cb8fbca375498e457f6d533781dfad6636f37208
Anna Borzenko <annnechko@gmail.com>
2024-03-01 11:54:55 +1300

    comments for attendance

--- app/src/Form/AttendanceType.php
+++ app/src/Form/AttendanceType.php
@@ -6,6 +6,7 @@ use App\Entity\Attendance;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,16 +14,43 @@ class AttendanceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $statusConfig = [
+            'label' => false,
+            'choices' => [
+                'Present' => Attendance::STATUS_PRESENT,
+                'Absent' => Attendance::STATUS_ABSENT,
+                'Excused' => Attendance::STATUS_EXCUSED,
+            ],
+        ];
+        $entityId = $builder->getData()?->getId();
+        if ($entityId === null) {
+            $statusConfig['placeholder'] = '';
+
+            $statusConfig['placeholder_attr'] = [
+                'data-role' => 'empty-value',
+            ];
+        } else {
+            $statusConfig['attr'] = [
+                'data-chosen' => $builder->getData()->getStatus(),
+            ];
+        }
+
+        $commentConfig = [
+            'label' => false,
+            'attr' => [
+                'class' => ' js-attendance-comment attendance-comment form-control form-control-sm',
+            ],
+        ];
+        if ($entityId !== null) {
+            if ($builder->getData()->getStatus() === Attendance::STATUS_PRESENT) {
+                $commentConfig['attr']['class'] .= ' hide';
+            }
+        } else {
+            $commentConfig['attr']['class'] .= ' hide';
+        }
         $builder
-            ->add('status', ChoiceType::class, [
-                'label' => false,
-                'choices' => [
-                    '' => null,
-                    'Present' => Attendance::STATUS_PRESENT,
-                    'Absent' => Attendance::STATUS_ABSENT,
-                    'Excused' => Attendance::STATUS_EXCUSED,
-                ],
-            ])
+            ->add('status', ChoiceType::class, $statusConfig)
+            ->add('comment', TextareaType::class, $commentConfig)
             ->add('studentName', HiddenType::class, [
                 'disabled' => true,
                 'mapped' => false,
