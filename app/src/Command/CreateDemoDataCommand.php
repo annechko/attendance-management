31afef5f7d9432c33f286c44021492d21379e58d
Anna Borzenko <annnechko@gmail.com>
2024-03-06 12:08:47 +1300

    demo command data

--- app/src/Command/CreateDemoDataCommand.php
+++ app/src/Command/CreateDemoDataCommand.php
@@ -41,14 +41,65 @@ class CreateDemoDataCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        if ($this->entityManager->getRepository(Institution::class)->findOneBy(
-            ['name' => 'Yoobee College of Creative Innovation']
-        )) {
-            $io->success(
-                'Already has data, skipping.'
-            );
+        //if ($this->entityManager->getRepository(Institution::class)->findOneBy(
+        //    ['name' => 'Yoobee College of Creative Innovation']
+        //)) {
+        //    $io->success(
+        //        'Already has data, skipping.'
+        //    );
+        //
+        //    return Command::SUCCESS;
+        //}
 
-            return Command::SUCCESS;
+        //Teachers
+        $teachers_data = [
+            [
+                'Oscar',
+                'Harrell',
+                'oscar.harrell@example.com',
+            ],
+            [
+                'Karen',
+                'Flynn',
+                'karen.flynn@example.com',
+            ],
+            [
+                'Donovan',
+                'Morton',
+                'donovan.morton@example.com',
+            ],
+            [//aut
+                'Ryan',
+                'Borenstein',
+                'ryan.borenstein@example.com',
+            ],
+            [
+                'Kendra',
+                'Markette',
+                'kendra.markette@example.com',
+            ],
+            [
+                'Finley',
+                'Naruaez',
+                'finley.naruaez@example.com',
+            ],
+
+        ];
+        $teachers = [];
+
+        foreach ($teachers_data as $entityData) {
+            $entity = new Teacher();
+            $teachers[] = $entity;
+            $entity->setName($entityData[0]);
+            $entity->setSurname($entityData[1]);
+            $entity->setEmail($entityData[2]);
+            $entity->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $entity,
+                    'teacher'
+                )
+            );
+            $this->entityManager->persist($entity);
         }
         // yoobee
         // aut
@@ -83,8 +134,21 @@ class CreateDemoDataCommand extends Command
         $this->entityManager->persist($course_y_dc);
 
         //courses aut
+
+        $course_a_ba = new Course();
+        $course_a_ba->setName('Bachelor of Architecture and Future Environments');
+        $course_a_ba->setDuration(new \DateInterval('P3Y'));
+        $course_a_ba->setInstitution($aut);
+        $this->entityManager->persist($course_a_ba);
+
+        $course_a_ms = new Course();
+        $course_a_ms->setName('Master of Engineering Project Management');
+        $course_a_ms->setDuration(new \DateInterval('P1Y'));
+        $course_a_ms->setInstitution($aut);
+        $this->entityManager->persist($course_a_ms);
+
         $subjects_mse = [
-            ['Professional Software Engineering', 'MSE800', 0, 1, 30],
+            ['Professional Software Engineering', 'MSE800', $period=0, $teacherIndex=1, $credits=30],
             ['Research Methods', 'MSE801', 0, 2, 15],
             ['Quantum Computing', 'MSE802', 0, 0, 15],
             ['Data Analytics', 'MSE803', 1, 0, 15],
@@ -104,36 +168,67 @@ class CreateDemoDataCommand extends Command
         }
 
         //Subjects aut
-
+        $subjects_data = [
+            ['Wānanga Design Studio I: Whakapapa/Relationships', 'ARCH500', $period=3, $teacherIndex=3, $credits=30],
+            ['Wānanga Design Studio II: Materials and Making', 'ARCH501', 3, 4, 30],
+            ['Architectural Intelligence I: Anthropocene', 'ARCH502', 3, 5, 15],
+            ['Architectural Communication', 'ARCH503', 3, 5, 15],
+            ['Architectural Ecologies I: Material Assemblies', 'ARCH504', 3, 5, 15],//year1
+            ['Wānanga Design Studio III: Mauri Ora I', 'ARCH600', 4, 3, 30],//year2
+            ['Wānanga Design Studio IV: Mauri Ora II', 'ARCH601', 4, 4, 30],
+            ['Architectural Intelligence II: The Pacific City', 'ARCH602', 4, 5, 30],
+            ['Architectural Ecologies II: Medium-Scale Construction', 'ARCH604', 4, 4, 30],
+        ];
+        $subjects_aut = [];
+        foreach ($subjects_data as $subjectData) {
+            $entity = new Subject();
+            $subjects_aut[] = $entity;
+            $entity->setName($subjectData[0]);
+            $entity->setCode($subjectData[1]);
+            $entity->setCourse($course_a_ms);
+            $this->entityManager->persist($entity);
+        }
 
         //Intakes yoobee
-        $intakes_mse_yoobee = [
+        $intakes_data = [
             [
                 'MSE 2311',
                 \DateTimeImmutable::createFromFormat('Y-m-d', '2023-11-13'),
                 \DateTimeImmutable::createFromFormat('Y-m-d', '2024-11-08'),
+                $course_y_mse
             ],
             [
                 'MSE 2307',
                 \DateTimeImmutable::createFromFormat('Y-m-d', '2023-07-13'),
                 \DateTimeImmutable::createFromFormat('Y-m-d', '2024-07-08'),
+                $course_y_mse
+            ],
+            [//index 2
+                'AK1337 2403',
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2024-03-03'),
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2027-03-03'),
+                $course_a_ba
+            ],
+            [
+                'AK1337 2503',
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2025-03-03'),
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2028-03-03'),
+                $course_a_ba
             ],
         ];
         $intakes = [];
-        foreach ($intakes_mse_yoobee as $intakeData) {
+        foreach ($intakes_data as $intakeData) {
             $intake = new Intake();
             $intakes[] = $intake;
             $intake->setName($intakeData[0]);
             $intake->setStart($intakeData[1]);
             $intake->setFinish($intakeData[2]);
-            $intake->setCourse($course_y_mse);
+            $intake->setCourse($intakeData[3]);
             $this->entityManager->persist($intake);
         }
 
-        //Intakes aut
-
         //Periods yoobee
-        $periods_mse_yoobee = [
+        $periods_data = [
             [
                 'Trimester 1',
                 \DateTimeImmutable::createFromFormat('Y-m-d', '2023-11-13'),
@@ -152,9 +247,21 @@ class CreateDemoDataCommand extends Command
                 \DateTimeImmutable::createFromFormat('Y-m-d', '2024-11-08'),
                 $intakes[0],
             ],
+            [//aut bachelor i3
+                'Year 1',
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2024-03-03'),
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2025-03-03'),
+                $intakes[2],
+            ],
+            [//aut bachelor i4
+                'Year 2',
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2025-03-03'),
+                \DateTimeImmutable::createFromFormat('Y-m-d', '2026-03-03'),
+                $intakes[2],
+            ],
         ];
         $periods = [];
-        foreach ($periods_mse_yoobee as $periodData) {
+        foreach ($periods_data as $periodData) {
             $period = new Period();
             $periods[] = $period;
             $period->setName($periodData[0]);
@@ -173,6 +280,13 @@ class CreateDemoDataCommand extends Command
             $entity->setTotalNumberOfLessons($subjectData[4]);
             $this->entityManager->persist($entity);
         }
+        foreach ($subjects_data as $index => $subjectData) {
+            $entity = new PeriodToSubject();
+            $entity->setSubject($subjects_aut[$index]);
+            $entity->setPeriod($periods[$subjectData[2]]);
+            $entity->setTotalNumberOfLessons($subjectData[4]);
+            $this->entityManager->persist($entity);
+        }
 
         //Students yoobee
         $studentsData = file_get_contents('/app/src/Command/students');
@@ -182,7 +296,7 @@ class CreateDemoDataCommand extends Command
             $studentItem = explode(',', $studentItem);
 
             $user = new Student();
-            $user->setIntake($intakes[0]);
+            $user->setIntake($intakes[rand(0,1)]);
             $user->setName($studentItem[1]);
             $user->setSurname($studentItem[2]);
             $user->setGender($studentItem[3]);
@@ -198,43 +312,27 @@ class CreateDemoDataCommand extends Command
         }
         //Students aut
 
-        //Teachers yoobee
-        $teachers_yoobee = [
-            [
-                'Oscar',
-                'Harrell',
-                'oscar.harrell@example.com',
-            ],
-            [
-                'Karen',
-                'Flynn',
-                'karen.flynn@example.com',
-            ],
-            [
-                'Donovan',
-                'Morton',
-                'donovan.morton@example.com',
-            ],
+        $studentsData = file_get_contents('/app/src/Command/students_aut');
+        $studentsData = explode("\n", $studentsData);
 
-        ];
-        $teachers = [];
+        foreach ($studentsData as $studentItem) {
+            $studentItem = explode(',', $studentItem);
 
-        foreach ($teachers_yoobee as $entityData) {
-            $entity = new Teacher();
-            $teachers[] = $entity;
-            $entity->setName($entityData[0]);
-            $entity->setSurname($entityData[1]);
-            $entity->setEmail($entityData[2]);
-            $entity->setPassword(
+            $user = new Student();
+            $user->setIntake($intakes[2]);
+            $user->setName($studentItem[1]);
+            $user->setSurname($studentItem[2]);
+            $user->setGender($studentItem[3]);
+            $user->setEmail($studentItem[0]);
+            $user->setPassword(
                 $this->userPasswordHasher->hashPassword(
-                    $entity,
-                    'teacher'
+                    $user,
+                    self::PASS
                 )
             );
-            $this->entityManager->persist($entity);
-        }
-        //Teachers aut
 
+            $this->entityManager->persist($user);
+        }
 
         //teacher subject intake
         foreach ($subjects_mse as $index => $subjectData) {
